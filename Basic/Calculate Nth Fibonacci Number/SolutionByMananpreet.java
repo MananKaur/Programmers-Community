ca215f4edfa7fa19eb57267363147d09b83ebe7e
Mananpreet Kaur <manankaur00@gmail.com>
2019-10-16 18:42:10 +0530

    Update SolutionByMananpreet.java

--- Basic/Calculate Nth Fibonacci Number/SolutionByMananpreet.java
+++ Basic/Calculate Nth Fibonacci Number/SolutionByMananpreet.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 class Fibonacci
+
 {
-	public static int fib(int n)
+	public static int first(int m)
 	{
+		int n = m-1;
+		return fib(n);
+	}
+	public static int fib(int n)
+	{     
+		
 		if(n<=1)
-		{
 			return n;
-		}
 
 		return fib(n-1)+fib(n-2);
 	}
@@ -15,6 +20,7 @@ class Fibonacci
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter a number");
 		int n =s.nextInt();
-		System.out.println("The "+ n+" th fibonacci number is : "+fib(n));
+		System.out.println("The "+ n+" th fibonacci number is : "+first(n));
 	}
 }
+
